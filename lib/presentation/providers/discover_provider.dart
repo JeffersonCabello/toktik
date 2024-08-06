7712d8801f6cc4de2cf0d127e01c58f22bcbac84
Jefferson Cabello <jefcsanc@espol.edu.ec>
2024-08-06 17:13:28 -0500

    No avance ni verga

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  //TODO Repositiry, DataSource
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
