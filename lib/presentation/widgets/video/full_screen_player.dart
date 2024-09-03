bb1bfe7fa2a3f3dc6c6004e326f962d5100f4b8e
JeffersonCabello <jefcsanc@espol.edu.ec>
2024-09-03 07:53:49 -0500

    nada

--- lib/presentation/widgets/video/full_screen_player.dart
+++ lib/presentation/widgets/video/full_screen_player.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class FullScreenPlayer extends StatelessWidget {
+class FullScreenPlayer extends StatefulWidget {
   final String videoUrl;
   final String caption;
-  
-  
-  const FullScreenPlayer({super.key, required this.videoUrl, required this.caption});
+
+  const FullScreenPlayer(
+      {super.key, required this.videoUrl, required this.caption});
+
+  @override
+  State<FullScreenPlayer> createState() => _FullScreenPlayerState();
+}
+
+class _FullScreenPlayerState extends State<FullScreenPlayer> {
+  late VideoPlayerController _controller;
 
   @override
   Widget build(BuildContext context) {
