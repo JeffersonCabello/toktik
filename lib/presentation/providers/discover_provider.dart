097f40b2ace041a8fb99ba8c7c99959a5e086086
JeffersonCabello <jefcsanc@espol.edu.ec>
2024-11-18 21:55:17 -0500

    Se acabo esta webada

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
-import 'package:toktik/infrastructure/models/local_video_model.dart';
-import 'package:toktik/shared/data/local_video_post.dart';
+import 'package:toktik/infrastructure/repositories/video_posts_repository_impl.dart';
 
 class DiscoverProvider extends ChangeNotifier {
-  //TODO Repositiry, DataSource
+  final VideoPostsRepositoryImpl videoPostRepository;
 
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
+  DiscoverProvider({required this.videoPostRepository});
+
   Future<void> loadNextPage() async {
     //todo: Cargar videos
-    await Future.delayed(const Duration(seconds: 2));
-    final List<VideoPost> newVideos = videoPosts
-        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
-        .toList();
+    //await Future.delayed(const Duration(seconds: 2));
+    //final List<VideoPost> newVideos = videoPosts
+    //  .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+    //.toList();
+
+    final newVideos = await videoPostRepository.getTrendingVideosByPage(1);
 
     videos.addAll(newVideos);
     initialLoading = false;
